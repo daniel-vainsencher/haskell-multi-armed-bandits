4a5c0ff13b605c4f3c6d98f9b49df2702d2485c7
Daniel Vainsencher <daniel.vainsencher@gmail.com>
2012-10-17 11:00:53 +0100

    Initial commit. Implements - the UCB-Tuned confidence bounds, using - a nice online mean-and-variance tracking algorithm. There is a first attempt to abstract the problem with - a type parameter, - a monad and - a type class, but the abstraction is probably all wrong.

---
+++ mab.hs
@@ -0,0 +1,70 @@
+import Data.List.Extras.Argmax
+import Data.List
+import Control.Monad.Random
+
+module MultiArmedBandit where
+
+
+--------- This section is a utility for maintaining empirical mean and variance estimates without keeping all scores.
+-- |Sufficient information to calculate online mean and variance, see
+-- |Donald E. Knuth (1998). The Art of Computer Programming, volume 2: Seminumerical Algorithms, 3rd edn., p. 232. Boston: Addison-Wesley.
+data Stats = OnlineMeanAndVariance {
+     mvN :: Integer,   -- number of samples so far
+     mvX,          -- current mean
+     mvM2 :: Float -- extra information for variance
+}
+
+-- |Stats for the empty sequence.
+emptyStats = OnlineMeanAndVariance {mvN = 0, mvX = 0, mvM2 = 0}
+
+-- |Add a number to the sequence over which we maintain statistics.
+withEntry :: Stats -> Float -> Stats
+withEntry (OnlineMeanAndVariance {mvN = n, mvX = x, mvM2 = m2}) xNew =
+       OnlineMeanAndVariance {mvN = n + 1,
+                              mvX = newMean,
+                              mvM2 = m2 + delta * (xNew - newMean) }
+          where
+                delta = xNew - x
+                newMean = x + delta / fromInteger (n + 1)
+-- |Number of entries in sequence
+entries OnlineMeanAndVariance {mvN = n} = n
+-- |Mean of entries
+mean OnlineMeanAndVariance {mvX = r} = r
+-- |Variance of entries
+variance OnlineMeanAndVariance {mvN = n, mvX = r, mvM2 = m2} = m2 / fromInteger n
+------- Utility ends here
+
+-- | Each bandit is characterized by its statistical properties of its scores (number, mean and variance), and by some opaque identity a seen only by the environment.
+data UCBBandits a = Bandits [(Stats, a)]
+
+-- | selfVisitStats, #totalVisits -> upper confidence bound
+ucbBeta :: Stats -> Integer -> Integer -> Float -> Float
+ucbBeta stats arms round beta = empMean + confidenceSlow + confidenceFast
+         where empMean = mean stats
+               confidenceSlow = sqrt (2 * variance stats * logPart / visit)
+	       confidenceFast = 16 * logPart / 3 / visit
+               visit = fromInteger $ entries stats
+               logPart = log $ 1 / betaS
+               betaS = beta / 4.0 / fromInteger arms / visit / (visit + fromInteger 1)
+
+class (Monad m) =>  BanditsProblem m where
+      payoff :: a -> m Float
+
+      play :: (m (UCBBandits a)) -> (m (UCBBandits a))
+      play bandits' = do
+          Bandits bandits <- bandits'
+          let
+              (allStats, allIDs) = unzip bandits
+              totalVisits = sum $ map entries allStats
+              arms = toInteger $ length bandits
+              currUCB s = ucbBeta s arms totalVisits 0.01
+              maxUCB = maximum $ map currUCB allStats
+              (best, rest) = partition (\(s,i) -> (currUCB s == maxUCB)) bandits
+              (chosenStats, chosenIdentity) = head best
+          newScore <- payoff chosenIdentity
+          let updatedArm = (chosenStats `withEntry` newScore, chosenIdentity)
+          return (Bandits (updatedArm : (tail best) ++ rest))
+
+instance BanditsProblem Rand where
+--  f x = min (abs (x - 1), abs (x + 1))
+  payoff arm = arm + getRandomR (0.0,1.0)
